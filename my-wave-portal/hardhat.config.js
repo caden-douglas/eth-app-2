75b85a6bca8b64eb61cb8c9b5b8b6c6c5f09fbf9
Joseph Douglas <joecdouglas2@gmail.com>
2022-12-05 21:53:41 -0500

    Changes to be committed: 	modified:   my-wave-portal/hardhat.config.js 	modified:   my-wave-portal/package-lock.json 	modified:   my-wave-portal/package.json

--- my-wave-portal/hardhat.config.js
+++ my-wave-portal/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -8,7 +9,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
